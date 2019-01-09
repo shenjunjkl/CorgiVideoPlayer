4817a631c76f55778509f39746853ed776819507
shenjun <805442858@qq.com>
2019-01-10 02:19:06 +0800

    add test video urls

---
+++ app/src/main/java/com/shenjun/corgivideoplayer/DemoVideoUrl.kt
@@ -0,0 +1,17 @@
+package com.shenjun.corgivideoplayer
+
+/**
+ * Created by shenjun on 2019/1/10.
+ */
+class DemoVideoUrl {
+
+    companion object {
+
+        const val MP4_1 = "https://media.w3.org/2010/05/sintel/trailer.mp4"
+        const val MP4_2 = "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
+        const val MP4_3 = "https://www.w3schools.com/html/movie.mp4"
+        const val MP4_4 = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
+
+    }
+
+}
